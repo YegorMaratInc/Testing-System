e860bb4dae98483702c9e118aaa62afec170af53
Rood1e <rodiveg@yandex.ru>
2020-03-12 15:34:50 +0300

    Fix project

--- src/main/java/ru/licpnz/testingsystem/security/details/UserDetailsServiceImpl.java
+++ src/main/java/ru/licpnz/testingsystem/security/details/UserDetailsServiceImpl.java
@@ -1,5 +1,11 @@
 package ru.licpnz.testingsystem.security.details;
 
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import ru.licpnz.testingsystem.repositories.UserRepository;
+
 /**
  * 28/11/2019
  * UserDetailsServiceImpl
@@ -7,20 +13,19 @@ package ru.licpnz.testingsystem.security.details;
  * @author havlong
  * @version 1.0
  */
-/**
 @Service
 public class UserDetailsServiceImpl implements UserDetailsService {
 
-private final UserRepository userRepository;
+    private final UserRepository userRepository;
 
-public UserDetailsServiceImpl(UserRepository userRepository) {
-this.userRepository = userRepository;
-}
+    public UserDetailsServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 
- @Override public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {
- return new
- UserDetailsImpl(userRepository.findUserByLogin(userName)
- .orElseThrow(IllegalArgumentException::new));
- }
+    @Override
+    public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {
+        return new
+                UserDetailsImpl(userRepository.findUserByLogin(userName)
+                .orElseThrow(IllegalArgumentException::new));
+    }
 }
- */
