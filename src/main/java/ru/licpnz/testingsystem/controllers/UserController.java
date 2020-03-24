6e8dd241ba5b5a9e6bb779d1be49a1e4aad318d0
Antony-Sk <skorkinanton@yandex.ru>
2020-03-24 22:53:05 +0300

    Now admins can add new ghosts!

--- src/main/java/ru/licpnz/testingsystem/controllers/UserController.java
+++ src/main/java/ru/licpnz/testingsystem/controllers/UserController.java
@@ -9,19 +9,24 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import ru.licpnz.testingsystem.exceptions.NotFoundException;
 import ru.licpnz.testingsystem.forms.UserEditForm;
+import ru.licpnz.testingsystem.forms.UserForm;
 import ru.licpnz.testingsystem.models.User;
 import ru.licpnz.testingsystem.models.UserRole;
 import ru.licpnz.testingsystem.models.UserState;
 import ru.licpnz.testingsystem.repositories.UserRepository;
+import ru.licpnz.testingsystem.services.SignUpService;
 
 @Controller
 public class UserController {
 
     private final UserRepository userRepository;
 
+    private final SignUpService signUpService;
+
     @Autowired
-    public UserController(UserRepository userRepository) {
+    public UserController(UserRepository userRepository, SignUpService signUpService) {
         this.userRepository = userRepository;
+        this.signUpService = signUpService;
     }
 
     @GetMapping("/users")
@@ -40,4 +45,22 @@ public class UserController {
         userRepository.save(user);
         return ResponseEntity.ok().build();
     }
+
+    @GetMapping("/createGhost")
+    public String getCreateGhostPage() {
+        return "createGhost";
+    }
+
+    @PostMapping("/createGhost")
+    public String createGhost(UserForm userCreateForm, ModelMap modelMap) {
+        if (userRepository.findUserByLogin(userCreateForm.getLogin()).isPresent()) {
+            modelMap.addAttribute("signupError", "true");
+            modelMap.addAttribute("alert", "Имя пользователя занято");
+            return "createGhost";
+        }
+        signUpService.signUp(userCreateForm, UserRole.GHOST);
+        return "redirect:/users";
+    }
+
+
 }
